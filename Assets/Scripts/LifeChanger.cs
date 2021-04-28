2d8c302d02694744c38faa210c088a717de8c20b
Ansar Batyrbayev <theansbob@gmail.com>
2021-04-28 15:30:11 +0600

    Update LifeChanger.cs

--- Assets/Scripts/LifeChanger.cs
+++ Assets/Scripts/LifeChanger.cs
@@ -1,37 +1,34 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LifeChanger : MonoBehaviour
+public class Life : MonoBehaviour
 {
     [SerializeField] private Slider _life;
 
-    private bool _increase;
-    private bool _decrease;
-
     private float _currentValue;
     private float _speed = 0.1f;
-    private float _distance = 0.1f;
+    private float _deltaLife;
 
     public void Change(bool increase)
     {
         if (increase)
-            _increase = true;
+            _deltaLife = 0.1f;
         else
-        {
-            _increase = false;
-            _decrease = true;
-        }
+            _deltaLife = -0.1f;
 
+        _currentValue = _life.value;
+    }
+
+    public void Awake()
+    {
+        _deltaLife = 0;
         _currentValue = _life.value;
     }
 
     public void Update()
     {
-        if (_increase)
-            _life.value = Mathf.MoveTowards(_life.value, _currentValue + _distance, Time.deltaTime * _speed);
-        else if (_decrease)
-            _life.value = Mathf.MoveTowards(_life.value, _currentValue - _distance, Time.deltaTime * _speed);
+        _life.value = Mathf.MoveTowards(_life.value, _currentValue + _deltaLife, Time.deltaTime * _speed);
     }
 }

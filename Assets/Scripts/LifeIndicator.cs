27b46ba6cc2ebdd86a5a3c674a5ea78df9988e6c
Ansar Batyrbayev <theansbob@gmail.com>
2021-05-13 06:56:05 +0600

    Update LifeIndicator.cs

--- Assets/Scripts/LifeIndicator.cs
+++ Assets/Scripts/LifeIndicator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Life))]
 public class LifeIndicator : MonoBehaviour
 {
     [SerializeField] private Slider _lifeSlider;
@@ -10,18 +11,25 @@ public class LifeIndicator : MonoBehaviour
     private float _currentValue;
     private float _speed = 0.1f;
 
-    private int _frames = 255;
+    private int _dividerForSlider = 100;
+
+    private Life _life;
+
+    private void Awake()
+    {
+        _life = gameObject.GetComponent<Life>();
+    }
 
     public void ChangeValue()
     {
-        _currentValue = Life.LifeValue / 100;
+        _currentValue = _life.LifeValue / _dividerForSlider;
 
         StartCoroutine(ChangeSliderValue());
     }
 
     public IEnumerator ChangeSliderValue()
     {
-        for (int i = 0; i < _frames; i++)
+        while (_lifeSlider.value != _currentValue)
         {
             _lifeSlider.value = Mathf.MoveTowards(_lifeSlider.value, _currentValue, Time.deltaTime * _speed);
 
